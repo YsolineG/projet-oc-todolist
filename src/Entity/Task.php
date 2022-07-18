1826872dc0a5261ac93cb72232b2f1dc91b649a9
YsolineG <ysoline.ganster@gmail.com>
2022-07-18 17:09:13 +0200

    feat: add isDone and createdAt when added a task

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -40,6 +40,12 @@ class Task
      */
     private $createdAt;
 
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->isDone = false;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
